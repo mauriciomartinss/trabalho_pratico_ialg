5104e7340ddd1223add7be42313e4c3fbecfd787
Hélio Henrique Medeiros Silva <helio.medeiros_silva@outlook.com>
2021-03-11 16:50:10 -0300

    --

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 using namespace std;
 //usei o pseudo código pra criar preencher os "objetos", mas acho que ao longo desenvolvimento vai precisar mudar kkkk
-struct precoProduto {
+struct produto
+{
+    string nome;
     int qtd;
     float valor;
 };
-struct cliente {
+struct cliente
+{
     string nome;
     int cpf;
     int saldoDevedor;
     int id;
 };
-struct cartao {
+struct cartao
+{
     int n;
     string nome;
     int cvv;
 };
-struct compra {
+struct compra
+{
     int id;
-    precoProduto produtos[];
+    produto produtos[];
     cliente clientes[100];
     string pagamento;
 };
 
-int menu_selecao() {
+int menu_selecao()
+{
     int acao;
-    cout << "Escolha uma acao (digite apenas inteiros): " <<endl
+    cout << "Escolha uma acao (digite apenas o numero): " << endl
          << "   1- Passar Compras" << endl
          << "   2- Exibir clientes cadastrados" << endl
          << "   3- cadastrar cliente" << endl
@@ -34,71 +40,127 @@ int menu_selecao() {
          << "   >";
     cin >> acao;
     cout << endl;
+
     return acao;
 }
 
-int passar_Compra(compra historico[]) {
+int passar_Compra(compra historico[])
+{
     compra compraAtual; //variavel que recebe tudo
     int i = 0;
-    while(compraAtual.id != 0) {
-        cin>>compraAtual.id;
-        while(compraAtual.produtos[i].qtd != 0) {
-            cin>>compraAtual.produtos[i].qtd;
-            if (compraAtual.produtos[i].qtd != 0) {
-                cin>>compraAtual.produtos[i].valor;
+    float valorTotal = 0;
+
+    /*while (compraAtual.produto[i].qtd != 0)
+    {
+        cin >> compraAtual.id;
+        while (compraAtual.produtos[i].qtd != 0)
+        {
+            cin >> compraAtual.produtos[i].qtd;
+            if (compraAtual.produtos[i].qtd != 0)
+            {
+                cin >> compraAtual.produtos[i].valor;
             }
-            cin>>compraAtual.pagamento; //nao terminei ainda, fiz algumas modificoes em duas estruturas
+            cin >> compraAtual.pagamento; //nao terminei ainda, fiz algumas modificoes em duas estruturas
         }
         i++;
+    }*/
+
+    cout << compraAtual.produtos[0].qtd << "Se nao deseja adicionar mais produto, digite 0";
+
+    if (compraAtual.produtos[0].qtd <= 0)
+    {
+        //encerra a função;
     }
-    
+    else
+    {
+
+        while (compraAtual.produtos[i].qtd >= 0)
+        {
+            cout << "nome do produto: ";
+            cin >> compraAtual.produtos[i].nome;
+            cout << endl
+                 << "preco unitario: ";
+            cin >> compraAtual.produtos[i].valor;
+
+            valorTotal = valorTotal + (compraAtual.produtos[i].valor * compraAtual.produtos[i].qtd);
+
+            cout
+                << compraAtual.produtos[i + 1].qtd << "Se nao deseja adicionar mais algum produto, digite 0";
+
+            i++;
+        }
+    }
+
+    cout << "O valor total da compra é de " << valorTotal << "." << endl
+         <<
+         //carrega as opcoes de pagamento disponiveis e faz um switch ou if para a resposta do usuario
+         << endl;
+
     int teste = 0;
     return teste;
 }
 
-void exibir_Clientes() {
+void exibir_Clientes()
+{
 }
 
-void cadastrar_Cliente() {
+void cadastrar_Cliente()
+{
 }
 
-void exibeHistorico(compra historico[]) {
+void exibeHistorico(compra historico[])
+{
     int i = 0;
-    while(i < 10 and historico[i].id != 0) {
-        cout<<historico[i].id<<endl
-            <<historico[i].clientes[i].id<<" "<<historico[i].clientes[i].saldoDevedor<<endl
-            <<historico[i].pagamento<<endl;
+    while (i < 10 and historico[i].id != 0)
+    {
+        cout << historico[i].id << endl
+             << historico[i].clientes[i].id << " " << historico[i].clientes[i].saldoDevedor << endl
+             << historico[i].pagamento << endl;
         i++;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-int main() {   
+int main()
+{
     compra historico[10]; //o histórico já tava definido que seria no máximo dez, né?
-
 
     int acao;
     /*  mudei pra menu selecao (antes era exibir acoes) pq, além de mostrar as acoes, ele também retorna 
         um inteiro que recebeu de entrada como acao. daí achei que seria melhor mudar o nome. mas eu acho que
         a gente pode melhorar ainda mais esses nomes hueuheuheuhuhe
-    */ 
+    */
     bool sair = false;
-    while(!sair) {
+    while (!sair)
+    {
         acao = menu_selecao();
-        if (acao == 1) {
+
+        if (acao == 1) //Hélio: acho que seria melhor usar switch;
+        {
             passar_Compra();
-        } else if (acao == 2) {
+        }
+        else if (acao == 2)
+        {
             exibir_Clientes();
-        } else if (acao == 3) {
+        }
+        else if (acao == 3)
+        {
             cadastrar_Cliente();
-        } else if (acao == 4) {
+        }
+        else if (acao == 4)
+        {
             exibeHistorico(historico);
-        } else if (acao == 5) {
+        }
+        else if (acao == 5)
+        {
             sair = true;
-        } else { //acho que assim eh melhor, se não for, depois muda :)
-            cout << "[ERRO] digite um valor válido! (valores de 1 a 5). " << endl << endl;
+        }
+        else
+        { //acho que assim eh melhor, se não for, depois muda :)
+            cout << "[ERRO] digite um valor válido! (valores de 1 a 5). " << endl
+                 << endl;
         }
     }
-    
+
     return 0;
 }
