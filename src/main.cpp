d2016d69556d60c8fda4a4a25223ef4a735270a0
mauriciomartinss <mauricio.damasceno@estudante.ufla.br>
2021-03-16 02:16:47 -0300

    simplificando o codigo (ficou so a struct cliente) adicionando outras coisas e comecando a mexer com arquivo

--- src/main.cpp
+++ src/main.cpp
@@ -1,116 +1,114 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 using namespace std;
 //Hélio Henrique Medeiros Silva and Maurício Martins Damasceno;
-struct produto
-{
-    string nome;
-    int qtd;
-    float valor;
-};
+
 struct cliente
 {
     string nome;
     int cpf;
-    int saldoDevedor;
-    int valorCompras;
-};
-struct compra
-{
-    produto produtos[100];
-    cliente clientes;
-    string pagamento;
+    float saldoDevedor;
+    float valorCompras;
+    
 };
 
-//void menuSelecao(int &acao)
-
-void menuSelecao()
-{
-    cout << "Escolha uma acao (digite apenas o numero): " << endl
-         << "   1- Passar Compras" << endl
-         << "   2- Exibir clientes cadastrados" << endl
-         << "   3- cadastrar cliente" << endl
-         << "   4- sair" << endl
-         << "   > ";
-    // cin >> acao;
+// ######################
+// aqui eu só coloquei a estrutura (copiei do slide de buscas). a gente tem que ver como 
+// vai procurar em um arquivo
+bool busca(cliente* clientes, int cpfCliente, int &posicaoCliente) {
+//     int K, N = 10; precisa implementar com o arquivo depois
+//     int V[N];
+//     int i, posicao = -1;
+//     for (i = 0; i < N; i++) cin >> V[i];
+//     cin >> K;
+//     while ((i < N) and (V[i] < K))
+//    	 i++;
+//     if ((i != N) and (V[i] == K)) {
+//    	 posicao = i;
+    //    } else {
+    //        return false
+    //    }
+    return true;
 }
 
-void passarCompra()
+void cadastrarCliente(cliente &dados)
 {
-    compra compraAtual; //variavel que recebe tudo
-    cliente cliente_atual;
-    int i = 0, pagamento, pgtCartao, valorRecebido, troco;
-    float valorTotal = 0;
+    cout << "nome: " << endl;
+    cin >> dados.nome;
+    cout << "cpf: " << endl;
+    cin >> dados.cpf;
+    dados.saldoDevedor = 0;
+    dados.valorCompras = 0;
+}
 
-    cout << "Informe a quantidade de um tipo de produto: ";
+void passarCompra(cliente* clientes, int tamanho)
+{
+    // compra compraAtual; //variavel que recebe tudo
+    cliente auxiliarCliente;
+    int pagamento;
+    // teste:
+    float valorProduto;
+    int i = 0;
+    float valorTotal = 0; //valor total da compra
+    // fim teste;
 
-    cin >> compraAtual.produtos[0].qtd;
-
-    if (compraAtual.produtos[0].qtd <= 0)
+    while (valorProduto != 0)
     {
-        return;
-    }
-    else
-    {
-        while (compraAtual.produtos[i].qtd > 0)
-        {
-            cout << "nome do produto: ";
-            cin >> compraAtual.produtos[i].nome;
-            cout << endl
-                 << "preco unitario: ";
-            cin >> compraAtual.produtos[i].valor;
-
-            valorTotal = valorTotal + (compraAtual.produtos[i].valor * compraAtual.produtos[i].qtd);
-
-            cout << "Informe a quantidade do produto. Se nao deseja adicionar mais algum produto, digite 0: ";
-
-            cin >> compraAtual.produtos[i + 1].qtd;
-
+            cout << "informe o preco do produto separado por ponto flutuante. Caso nao haja mais produtos, digite 0."<<endl
+                 << ">";
+            cin >> valorProduto;
+            valorTotal += valorProduto;
             i++;
-        }
     }
 
-    cout << "O valor total da compra é de " << valorTotal << "." << endl
+    cout << "O valor total da compra é de " << auxiliarCliente.saldoDevedor << "." << endl
          << "Qual a forma de pagamento? " << endl
-         << "1- Cartao" << endl
-         << "2- Dinheiro" << endl
-         << "3- À prazo" << endl;
+         << "1- À vista" << endl
+         << "2- À prazo" << endl;
     cin >> pagamento;
 
     //  arquivo com lista de clientes;
     //  verifica_cliente();
     //  senao
     //  cadastrar_cliente();
-
+    int cadastradoOuNao;
     if (pagamento == 1)
     {
-        cout << "1- À vista" << endl
-             << "2- Parcelado" << endl;
-        cin >> pgtCartao;
-
-        if (pgtCartao == 1)
-        {
-            cout << "Confirmado";
-        }
-        else if (pgtCartao == 2)
-        {
-            cout << "quantas vezes?" << endl
-                 << "Confirmado";
-        }
+        cout<<"Processo finalizado";
     }
     else if (pagamento == 2)
     {
-        cout << "valor recebido: ";
-        cin >> valorRecebido;
-        troco = valorRecebido - valorTotal;
-
-        cout << endl
-             << "troco: " << troco << endl;
-    }
-    else if (pagamento == 3)
-    {
-        //clienteAtual.saldoDevedor = clienteAtual.saldoDevedor+valorTotal;
+        int cpfCliente;
+        int posicaoCliente;
+        cout<< "o cliente é cadastrado? "<<endl
+            << "1-sim"<<endl
+            << "2-nao"<<endl;
+        cin>> cadastradoOuNao;
+        bool sair = false;
+        while(!sair) {
+            if (cadastradoOuNao >= 1 and cadastradoOuNao <= 2){
+            if(cadastradoOuNao == 1) {
+                cout<<"Digite o CPF do cliente: ";
+                cin>>cpfCliente;
+                if (busca(clientes, cpfCliente, posicaoCliente)) { //vai retornar um true
+                    clientes[posicaoCliente].saldoDevedor += auxiliarCliente.saldoDevedor;
+                } else {
+                    cout<<"Nao eh possivel passar a prazo sem ser cadastrado. Por favor, cadastre o cliente e depois passe a compra novamente!"<<endl;
+                    sair = true;
+                }
+            } else {
+                cout<<"Nao eh possivel passar a prazo sem ser cadastrado. Por favor, cadastre o cliente e depois passe a compra novamente!"<<endl;
+                sair = true;
+            }
+            } else {
+                cout << "[ERRO] digite um valor valido! (1 ou 2) " << endl;
+                cin.clear();  //Limpa a flag de erro quando há falha no parse do valor entrado
+                cin.ignore(); //Limpa o buffer
+            }    
+        }
+        
     }
 }
 
@@ -120,20 +118,18 @@ void interfaceCliente()
          << "   2- Excluir cliente" << endl
          << "   3- Alterar dado de cliente" << endl
          << "   4- Retornar ao menu principal" << endl
-         << "   >";
+         << "   > ";
 }
 
-void carregarClientes()
+void cadastrarCliente(cliente* clientes, int tamanho)
 {
-    ifstream cli("clientes.dat", ios::binary);
-}
-
-void cadastrarCliente(cliente &dados)
-{
+    cliente clientes[tamanho-1];
     cout << "nome: " << endl;
-    cin >> dados.nome;
+    cin >> clientes[tamanho-1].nome;
     cout << "cpf: " << endl;
-    cin >> dados.cpf;
+    cin >> clientes[tamanho-1].cpf;
+    clientes[tamanho-1].saldoDevedor = 0;
+    clientes[tamanho-1].valorCompras = 0;
 }
 
 void quitarDivida()
@@ -148,7 +144,7 @@ void alterarCliente()
 {
 }
 
-void exibirClientes()
+void exibirClientes(cliente* clientes, int tamanho)
 {
     bool sair = false;
     int opcaoCliente;
@@ -167,7 +163,7 @@ void exibirClientes()
             {
                 if (opcaoCliente == 1)
                 {
-                    //cadastrarCliente();
+                    cadastrarCliente(clientes, tamanho);
                 }
                 else if (opcaoCliente == 2)
                 {
@@ -188,16 +184,16 @@ void exibirClientes()
     }
 }
 
-void verificar(int acao)
+void verificar(int acao, cliente* clientes, int tamanho)
 {
 
     if (acao == 1)
     {
-        passarCompra();
+        passarCompra(clientes, tamanho);
     }
     else if (acao == 2)
     {
-        exibirClientes();
+        exibirClientes(clientes, tamanho);
     }
     else if (acao == 3)
     {
@@ -210,8 +206,39 @@ void verificar(int acao)
     }
 }
 
+void menuSelecao()
+{
+    cout << "Escolha uma acao (digite apenas um numero): " << endl
+         << "   1- Passar Compras" << endl
+         << "   2- Exibir clientes cadastrados" << endl
+         << "   3- Cadastrar cliente" << endl
+         << "   4- Quitar Divida" <<endl
+         << "   5- Sair" << endl
+         << "   > ";
+}
+
+// #####################################################################
+// aqui foi o último lugar que eu mexi
+void salvarDados(cliente* clientes, int tamanho) {
+    // como tá tudo em string ou inteiro, a gente precisa passar pra char;
+    ofstream arquivo("dados.dat", ios::binary);
+    char caractere[1];
+
+    //escreve o nome
+    for(int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < (clientes[i].nome).size(); i++) {
+            caractere[0] = clientes[i].nome[j];
+            arquivo.write(caractere,1);
+        }
+    }
+
+}
+
 int main()
 {
+    int tamanho = 1;
+    cliente* clientes = new cliente[tamanho];
+    // carregarClientes(clientes, tamanho); talvez a gente não precise colocar na memoria mesmo
     int acao = 0;
     bool sair = false;
 
@@ -219,7 +246,6 @@ int main()
     {
         menuSelecao();
         cin >> acao;
-        cout << acao << endl;
         if (acao >= 1 and acao <= 5)
         {
             if (acao == 5)
@@ -228,7 +254,7 @@ int main()
             }
             else
             {
-                verificar(acao);
+                verificar(acao, clientes, tamanho);
             }
         }
         else
@@ -239,6 +265,8 @@ int main()
         }
 
     } //fim do while
+
+    salvarDados(clientes, tamanho);
 
     return 0;
 }
